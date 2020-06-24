8d59d4f84fed30950f76fd706424a5fc4b22dc03
andreyryabchenko <andrereb1@gmail.com>
2020-06-24 20:37:52 +0700

    x

--- js/common.js
+++ js/common.js
@@ -1,9 +1,12 @@
-// $(function(){
-//
-// });
-// $(window).load(function() {
-//
-// });
-// $(window).resize(function(){
-//
-// });
+//import  simpleParallax from '../node_modules/simple-parallax-js/dist/simpleParallax.js';
+
+
+
+
+var images = document.querySelectorAll('.thumbnail');
+new simpleParallax(images, {
+    delay: 0,
+    orientation: 'down',
+    scale: 1.3,
+    overflow: true,
+});
