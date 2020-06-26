73f2534789f7895e6ae897735bf1d5dad53da566
Андрей Рябченко <ryabchenko.a@delcomp.ru>
2020-06-26 15:24:38 +0700

    'x'

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,7 @@
 import {event, toggleClass} from "./functions.js";
 
+//header-burger
 event('.header .burger', 'click', function (e) {
-
     e.preventDefault();
     toggleClass(this, 'active');
     if (window.matchMedia("(max-width: 949px)").matches) {
@@ -10,17 +10,9 @@ event('.header .burger', 'click', function (e) {
     }
     toggleClass(document.querySelector('.header__mobile-menu'), 'vis');
 
-
     document.querySelectorAll('.overlayout').forEach((el) => {
         toggleClass(el, 'active');
     });
-
-
-
-    if(document.querySelector('.header__lng').classList.contains('active')){
-    }
-
-
 });
 
 window.addEventListener('resize', function () {
@@ -29,14 +21,49 @@ window.addEventListener('resize', function () {
     }
 }, true);
 
-
 if (document.querySelector('.header')) {
     document.addEventListener('click', function (event) {
         let menu = document.querySelector('.header__mobile-menu');
         let menuBtn = document.querySelector('.header');
         let isClickInside = menuBtn.contains(event.target);
         if (!isClickInside && menu.classList.contains('vis')) {
-            menuBtn.querySelector('.burger').click();
+            menuBtn.querySelector('.header .burger').click();
         }
     });
 }
+
+
+
+
+//footer-burger
+event('.footer .burger', 'click', function (e) {
+    e.preventDefault();
+    toggleClass(this, 'active');
+    if (window.matchMedia("(max-width: 949px)").matches) {
+        // toggleClass(document.querySelector('html'), 'overflow-hide');
+        // toggleClass(document.body, 'overflow-hide');
+    }
+    toggleClass(document.querySelector('.footer__mobile-menu'), 'vis');
+
+    document.querySelectorAll('.overlayout').forEach((el) => {
+        toggleClass(el, 'active');
+    });
+});
+
+window.addEventListener('resize', function () {
+    if (document.querySelector('.footer .burger').classList.contains('active')) {
+        document.querySelector('.footer .burger').click();
+    }
+}, true);
+
+if (document.querySelector('.footer')) {
+    document.addEventListener('click', function (event) {
+        let menu = document.querySelector('.footer__mobile-menu');
+        let menuBtn = document.querySelector('.footer');
+        let isClickInside = menuBtn.contains(event.target);
+        if (!isClickInside && menu.classList.contains('vis')) {
+            menuBtn.querySelector('.footer .burger').click();
+        }
+    });
+}
+
--- js/lng-btn.js
+++ js/lng-btn.js
@@ -1,5 +1,5 @@
 import {event, toggleClass} from "./functions.js";
-event('.header__lng', 'click', function (e) {
+event('.header .header__lng', 'click', function (e) {
     toggleClass(this, 'active');
 
     if(document.querySelector('.burger').classList.contains('active')){
@@ -18,28 +18,11 @@ if (document.querySelector('.header__profile .header__lng')) {
             menuBtn.click();
         }
     });
-
-
 }
-
-// if (document.querySelector('.header__mobile-menu .header__lng')) {
-//     document.addEventListener('click', function (event) {
-//         let menu = document.querySelector('.header__mobile-menu .header__lng');
-//         let menuBtn = document.querySelector('.header__mobile-menu .header__lng');
-//         let isClickInside = menu.contains(event.target);
-//         if (!isClickInside && menuBtn.classList.contains('active')) {
-//             menuBtn.click();
-//         }
-//     });
-// }
 
 
 window.addEventListener('resize', function () {
     if (document.querySelector('.header__profile .header__lng').classList.contains('active')) {
         document.querySelector('.header__profile .header__lng').click();
     }
-
-    // if (document.querySelector('.header__mobile-menu .header__lng').classList.contains('active')) {
-    //     document.querySelector('.header__mobile-menu .header__lng').click();
-    // }
 }, true);
