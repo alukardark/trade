c8025d815cb0c34ed730847f05a2ee5555a960b7
andreyryabchenko <andrereb1@gmail.com>
2020-06-27 17:42:01 +0700

    x

--- js/parallax.js
+++ js/parallax.js
@@ -41,3 +41,26 @@ new simpleParallax(document.querySelectorAll('.parallax--4'), {
     scale: 1.8,
     overflow: true,
 });
+
+
+
+
+new simpleParallax(document.querySelectorAll('.parallax--7'), {
+    delay: 0,
+    orientation: 'up',
+    scale: 2.1,
+    overflow: true,
+});
+
+new simpleParallax(document.querySelectorAll('.parallax--8'), {
+    delay: 0,
+    orientation: 'up',
+    scale: 1.3,
+    overflow: true,
+});
+new simpleParallax(document.querySelectorAll('.parallax--9'), {
+    delay: 0,
+    orientation: 'up',
+    scale: 1.5,
+    overflow: true,
+});
